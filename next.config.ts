1e2ab4578e39accaefe7459ccd077b8dcde837c7
Wess-codes <westrinwaithaka04@gmail.com>
2026-03-19 00:01:22 +0300

    Add Resend integration to consultation form

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Enable static export
-  output: 'export',
-  // You can add other config options here if needed
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
